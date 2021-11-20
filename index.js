b70ea131be3a984d070c1e880fc81fff3a0139c2
Ademu Anthony <ademuanthony@gmail.com>
2021-11-20 13:42:33 +0100

    Fe cleanup

--- index.js
+++
@@ -1,6 +0,0 @@
-﻿import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
-
-window.Stimulus = Application.start()
-const context = require.context("./js/controllers", true, /\.js$/)
-Stimulus.load(definitionsFromContext(context))
